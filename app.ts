0928c06c7abe7f66880ea4b0d22f44b5692ac724
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2020-08-05 19:27:22 +0200

    TypeScript files vs JavaScript files

--- app.ts
+++ app.ts
@@ -1,4 +1,9 @@
-const add = (n1: number, n2: number) => n1 + n2;
+const add = (n1: number, n2: number) => {
+	// if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+	// 	throw new Error('Inccorect input!');
+	// }
+	return n1 + n2;
+};
 
 const number1 = 5;
 const number2 = 2.8;
