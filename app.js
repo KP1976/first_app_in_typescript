37a05063c058164db24343c8fd8e98a2ba4ce176
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2020-08-06 19:58:20 +0200

    Tuple

--- app.js
+++ app.js
@@ -5,12 +5,15 @@
 var person = {
     name: 'Krzysztof',
     age: 44,
-    hobbies: ['Sport', 'Gotowanie']
+    hobbies: ['Sport', 'Gotowanie'],
+    role: [2, 'autor']
 };
+person.role.push('admin');
+person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ['Sport'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toUpperCase());
 }
--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 // 	name: string;
 // 	age: number;
 // } = {
-const person = {
+const person: {
+	name: string;
+	age: number;
+	hobbies: string[];
+	role: [number, string];
+} = {
 	name: 'Krzysztof',
 	age: 44,
 	hobbies: ['Sport', 'Gotowanie'],
+	role: [2, 'autor'],
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sport'];
